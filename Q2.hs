b8391f1b46b6e60eed90684b1fe9300f21bf977a
Adam Wang <adw24@ic.ac.uk>
2024-12-02 07:55:57 +0000

    Combine guard cases in decreasing and increasing

--- Q2.hs
+++ Q2.hs
@@ -8,18 +8,25 @@ numLines = 1000
 decreasing :: [Int] -> Bool
 decreasing [l] = True
 decreasing (l1:l2:ls)
-  | diff > 3  = False
-  | diff < 1  = False
-  | otherwise = decreasing (l2:ls)
+  | diff > 3 || diff < 1 = False
+  | otherwise            = decreasing (l2:ls)
+  where diff = l1 - l2
+
+dampedDecreasing :: [Int] -> Bool -> Bool
+dampedDecreasing (l1:l2:ls) True
+  | diff > 3 || diff < 1 = undefined
+  | otherwise = dampedDecreasing (l2:ls) True
   where diff = l1 - l2
 
 increasing :: [Int] -> Bool
 increasing [l] = True
 increasing (l1:l2:ls)
-  | diff > 3  = False
-  | diff < 1  = False
-  | otherwise = increasing (l2:ls)
+  | diff > 3 || diff < 1 = False
+  | otherwise            = increasing (l2:ls)
   where diff = l2 - l1
+
+dampedIncreasing :: [Int] -> Bool -> Bool
+dampedIncreasing = undefined
 
 main :: IO ()
 main = do
